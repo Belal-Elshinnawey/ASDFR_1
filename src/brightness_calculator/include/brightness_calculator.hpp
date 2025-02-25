6aed0b7f812055bf34828aee96c73bbfc20c0889
Nicholas Ehsan Roy <nicholas.ehsan.roy@gmail.com>
2025-02-25 16:22:59 +0100

    pulled brightness calculator from belal_branch

--- src/brightness_calculator/include/brightness_calculator.hpp
+++ src/brightness_calculator/include/brightness_calculator.hpp
@@ -1,18 +1,15 @@
 #ifndef BRIGHTNESS_CALCULATOR_HPP
 #define BRIGHTNESS_CALCULATOR_HPP
 
-#include "cv_bridge/cv_bridge.hpp"
-
 #include <boost/asio.hpp>
+#include <opencv2/opencv.hpp>
 
+#include "cv_bridge/cv_bridge.hpp"
 #include "rcl_interfaces/msg/parameter_descriptor.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_components/register_node_macro.hpp"
-#include "std_msgs/msg/bool.hpp"
 #include "sensor_msgs/msg/image.hpp"
-
-#include <opencv2/opencv.hpp>
-
+#include "std_msgs/msg/bool.hpp"
 
 namespace brightness_calculator {
 class BrightnessCalculator : public rclcpp::Node {
@@ -24,12 +21,7 @@ class BrightnessCalculator : public rclcpp::Node {
   void initialize();
   double calculate_brightness(const cv::Mat &image);
   int32_t brightness_threshold_;
-  enum BrightnessStatus
-  {
-      DARK,
-      LIGHT,
-      UNKNOWN
-  };
+  enum BrightnessStatus { DARK, LIGHT, UNKNOWN };
   BrightnessStatus is_light_;
   size_t width_;
   size_t height_;
@@ -37,15 +29,15 @@ class BrightnessCalculator : public rclcpp::Node {
   std::string durability_;
   std::string history_;
   size_t depth_;
-  enum PublishMode
-  {
-      CONTINUOUS,
-      ON_CHANGE
-  };
+  enum PublishMode { CONTINUOUS, ON_CHANGE };
   PublishMode publish_mode_;
-  
-  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
-  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr pub_;
+  bool show_camera_;
+
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_brightness_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_filter_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr pub_brightness_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_filter_;
+
 };
 
 }  // namespace brightness_calculator
